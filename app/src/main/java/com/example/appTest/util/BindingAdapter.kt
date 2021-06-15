e27d58f4bd42b2c6925ce42126de0736098b40ed
DESKTOP-NUG1B8M\Bui Quang Tan Hy <tanhy411998@gmail.com>
2021-06-15 19:11:25 +0700

    Update Load Image

--- app/src/main/java/com/example/appTest/util/BindingAdapter.kt
+++ app/src/main/java/com/example/appTest/util/BindingAdapter.kt
@@ -1,9 +1,13 @@
 package com.example.appTest.util
 
 import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import coil.load
 import com.bumptech.glide.Glide
+import com.example.appTest.R
 import com.example.appTest.ui.AppTextApplication.Companion.appContext
+import com.squareup.picasso.Picasso
 
 @BindingAdapter("bindingImageResource")
 fun bindingImageResource(view: ImageView, icon: Int) {
@@ -18,8 +22,22 @@ fun bindingImageUrl(view: ImageView, url: String) {
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
     imgUrl?.let {
-        Glide.with(appContext)
-            .load(imgUrl)
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
             .into(imgView)
     }
 }
+@BindingAdapter("imageUrlCoil")
+fun bindingImageUrlCoil(imgView: ImageView, imgUrl: String){
+    imgView.load(imgUrl)
+}
+
+//fun bindImagePicasso(imgView: ImageView, imgUrl: String?){
+//    Picasso.get()
+//        .load(imgUrl)
+//        .placeholder(R.drawable.placeholder)
+//        .error(R.drawable.error)
+//        .fit()
+//        .into(imageView)
+//}
